41b9996c16ba6b9fe334fd2bc278aee2b87f9b00
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-27 10:17:36 +0530

    [chiru] Add Checking OrderList

---
+++ OrderAndUnOrder/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderAndUnOrder
+{
+    internal class Node
+    {
+        public string Data;
+        public Node next;
+
+        public Node(string data)
+        {
+            this.Data = data;
+
+
+        }
+    }
+}
+    
+
---
+++ OrderAndUnOrder/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace OrderAndUnOrder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            OrderList orderList = new OrderList();
+
+            Console.WriteLine("Enter word to search");
+            string word = Console.ReadLine();
+            orderList.Ordered(word);
+
+        }
+    }
+}
